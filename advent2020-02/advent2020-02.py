d9d0e7426410e5b5f51aa50828e30fae74991c6e
maphestus <lockecmu@gmail.com>
2020-12-03 17:24:05 -0500

    Day 2 Part 2 commit

--- advent2020-02/advent2020-02.py
+++ advent2020-02/advent2020-02.py
@@ -1,13 +1,16 @@
 
-password_file = open("advent2020-02-input-ex.txt", "r")
+password_file = open("advent2020-02-input.txt", "r")
 
 tot_valid = 0
-
+tot_pt2 = 0
+# PART 1 START
 for password in password_file:
     li = password.split()
     pass_range = li[0].split("-")
     low_bound = int(pass_range[0])
+    low_pt2 = int(pass_range[0])-1
     up_bound = int(pass_range[1])
+    up_pt2 = int(pass_range[1])-1
     letter = li[1].strip(":")
     corrupt = li[2]
     tot_letter = 0
@@ -15,7 +18,17 @@ for password in password_file:
         if le == letter:
             tot_letter = tot_letter + 1
     if low_bound <= tot_letter <= up_bound:
-        print(corrupt, "is valid!")
+        print(corrupt, "is valid for part 1!")
         tot_valid = tot_valid + 1
+    # PART 2 START
+    if (corrupt[low_pt2] == letter and corrupt[up_pt2] != letter) or (corrupt[up_pt2] == letter and corrupt[low_pt2] != letter):
+        print(corrupt, "is valid for part 2!")
+        tot_pt2 = tot_pt2 + 1
+    # PART 2 END
 
-print("Total valid passwords:", tot_valid)
+print("Total valid passwords for part 1:", tot_valid)
+print("Total valid passwords for part 2:", tot_pt2)
+
+password_file.close()
+
+# PART 1 END
