476298176b6052f89f2a6c3d319d37f7b8585166
Seth <lockecmu@gmail.com>
2020-12-05 10:03:43 -0500

    Day 3 Part 2 commit

--- advent2020-03/advent2020-03.py
+++ advent2020-03/advent2020-03.py
@@ -30,3 +30,38 @@ while sled[0] < hill_height:
     print("sled is on", hill[y][x])
 
 print("Total trees encountered:", tot_trees)
+
+# PART 2
+
+
+def tree_collision(hill_map, height, width, travel_y, travel_x):
+    total_trees = 0
+    sled_pos = [0, 0]
+    # print(hill_map)
+    # print(height, width)
+    # print(travel_y, travel_x)
+
+    while sled_pos[0] < height and (sled_pos[0] + travel_y) <= height:
+        sled_pos[1] = sled_pos[1] + travel_x
+        if sled_pos[1] >= width:
+            sled_pos[1] = sled_pos[1] - width
+        sled_pos[0] = sled_pos[0] + travel_y
+        pos_y = sled_pos[0]
+        pos_x = sled_pos[1]
+        if hill_map[pos_y][pos_x] == "#":
+            total_trees = total_trees + 1
+        # print(sled_pos)
+        # print("sled is on", hill_map[pos_y][pos_x])
+    print(total_trees)
+    return total_trees
+
+
+run1 = tree_collision(hill, hill_height, hill_width, 1, 1)
+run2 = tree_collision(hill, hill_height, hill_width, 1, 3)
+run3 = tree_collision(hill, hill_height, hill_width, 1, 5)
+run4 = tree_collision(hill, hill_height, hill_width, 1, 7)
+run5 = tree_collision(hill, hill_height, hill_width, 2, 1)
+
+trees = run1 * run2 * run3 * run4 * run5
+
+print("Total trees multiplied together: ", trees)
